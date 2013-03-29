0f8247554efa212525bac1fd9c3903d6013a601b
beginor <beginor@qq.com>
2013-03-30 07:33:42 +0800

    enable package restore

---
+++ Test/HttpTestLib/TestEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpTestLib {
+
+	public class TestEntity {
+
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+
+		public DateTime Birthday { get; set; }
+
+		public static TestEntity[] CreateSamples() {
+			return new[] {
+				new TestEntity {
+					Id = 1,
+					Name = "Entity 1",
+					Age = 1,
+					Birthday = new DateTime(1900, 1, 1)
+				},
+				new TestEntity {
+					Id = 2,
+					Name = "Entity 2",
+					Age = 1,
+					Birthday = new DateTime(1900, 2, 1)
+				},
+				new TestEntity {
+					Id = 3,
+					Name = "Entity 3",
+					Age = 3,
+					Birthday = new DateTime(1900, 3, 1)
+				}
+			};
+		}
+
+	}
+}
---
+++ Test/HttpTestWeb/Controllers/BrowserHttpController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.Linq;
+using HttpTestLib;
+
+namespace HttpTestWeb.Controllers {
+
+	public class BrowserHttpController : ApiController {
+
+		private static IList<TestEntity> _data = TestEntity.CreateSamples();
+
+		public IEnumerable<TestEntity> Get() {
+			return _data;
+		}
+
+		public int Post(TestEntity newEntity) {
+			var id = _data.Max(e => e.Id) + 1;
+			newEntity.Id = id;
+			_data.Add(newEntity);
+			return id;
+		}
+	}
+}
